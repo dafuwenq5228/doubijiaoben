7314ac0ab9a0f67b4717a31158eddda951e2875a
dafuwenq5228 <58673502+dafuwenq5228@users.noreply.github.com>
2021-06-03 11:16:48 +0800

    Update caddy_install.sh

--- caddy_install.sh
+++ caddy_install.sh
@@ -54,8 +54,10 @@ Download_caddy(){
 		wget --no-check-certificate -O "caddy_v1.0.4_linux_amd64.tar.gz" "https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_amd64.tar.gz${extension_all}"
 	elif [[ ${bit} == "i386" || ${bit} == "i686" ]]; then
 		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_386.tar.gz${extension_all}"
-	elif [[ ${bit} == "armv7l" ]]; then
+	elif [[ ${bit} == "armv7" ]]; then
 		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_arm7.tar.gz${extension_all}"
+	elif [[ ${bit} == "arm64" ]]; then
+		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_arm64.tar.gz${extension_all}"
 	else
 		echo -e "${Error_font_prefix}[错误]${Font_suffix} 不支持 [${bit}] ! 请向本站反馈[]中的名称，我会看看是否可以添加支持。" && exit 1
 	fi
